28171bca09665532b8a2adff890f33567047e810
Arpit Yadav <142081471+ArpitSyntaxError@users.noreply.github.com>
2024-03-05 21:14:43 +0530

    add changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Signup from "./auth/SignUp";
+import Login from "./auth/Login";
+import { Route, Routes } from "react-router-dom";
+import NoPageFound from "./NoPageFound";
+import Navbar from "./parts/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      {/* <Signup /> */}
+      <Login />
+      {/* <Routes> */}
+      {/* <Route path="/" element={<Signup />} />
+      <Route path="login" element={<Login />} />
+      <Route path="*" element={<NoPageFound />} /> */}
+      {/* </Routes> */}
+    </>
   );
 }
 
